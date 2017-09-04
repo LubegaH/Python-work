2fd019960363d60d6a646c1d6170b8e155cd9eef
LubegaH <lubegah@gmail.com>
2017-09-04 15:56:15 +0300

    Calculates volume of sphere

---
+++ vol.py
@@ -0,0 +1,11 @@
+# Fuctions and methods homework from the Python boot camp class
+# Function computes volume of sphere given its radius
+
+from math import pi,pow
+
+# Volume of sphere = 4/3pir3
+def vol(rad):
+	v = (4.0/3)*pi*pow(rad,3)
+	print v
+
+vol(4)
